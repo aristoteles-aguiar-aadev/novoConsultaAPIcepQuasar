3f0c316eb83aa8da347af558186b913911225304
aristoteles-aguiar-aadev <aristotelesouza@gmail.com>
2023-02-11 17:41:19 -0300

    Nova tela de consulta a API viacep com Quasar Framework

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,97 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Consulta CEP </q-toolbar-title>
+
+        <div style="height:5em"></div>
+        <q-avatar>
+          <img src="../assets/consultaCEPwhite.png" alt="consultaCepWhite">
+        </q-avatar>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <img style="margin: 4em 0 0 8em;" src="../assets/consultaCEP.png"
+      />
+      <q-list>
+        <q-item-label header style="margin: 3em 0 2em 2em; color: #96CEB4; font-size: 20px;">Links - Portfólio</q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+          style="color:#96CEB4"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+{
+    title: 'Portfólio',
+    caption: 'Portfólio',
+    icon: 'pages',
+    link: 'https://portfolio-aristotelesaguiar.netlify.app/'
+  },
+  {
+    title: 'Linkedin',
+    caption: 'in/aristoteles-aguiar',
+    icon: 'face',
+    link: 'https://www.linkedin.com/in/aristoteles-aguiar/'
+  },
+  {
+    title: 'GitHub',
+    caption: 'aristoteles-aguiar-aadev',
+    icon: 'code',
+    link: 'https://github.com/aristoteles-aguiar-aadev'
+  },
+
+  {
+    title: 'Contatos',
+    caption: 'Telefone | Whatsapp',
+    icon: 'phone',
+    link: 'https://api.whatsapp.com/send?phone=5585981066622'
+  },
+]
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,134 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md" style="max-width: 400px">
+      <div class="row">
+        <q-form @submit="onSubmit" @reset="onReset" class="q-col-gutter">
+          <div class="input">
+            <q-input
+              class="cep"
+              rounded
+              standout
+              bottom-slots
+              v-model="cep"
+              label="CEP"
+              hint="CEP"
+              lazy-rules
+              :rules="[
+                (val) =>
+                  (val && val.length > 8) || 'Insira o CEP a ser consultado',
+              ]"
+            >
+              <template v-slot:prepend>
+                <q-icon name="place" />
+              </template>
+            </q-input>
+
+            <q-separator style="background-color: #96ceb4" />
+            <p></p>
+
+            <q-input
+              outlined
+              class="col1"
+              rounded
+              standout
+              bottom-slots
+              v-model="logradouro"
+              label="Logradouro"
+              hint="Avenida | Rua"
+            >
+            </q-input>
+
+            <q-separator style="background-color: #96ceb4" />
+            <p></p>
+
+            <q-input
+              outlined
+              rounded
+              standout
+              bottom-slots
+              v-model="complemento"
+              label="Complemento"
+              hint="Complemento | Referência"
+            >
+            </q-input>
+
+            <q-separator style="background-color: #96ceb4" />
+            <p></p>
+
+            <q-input
+              outlined
+              rounded
+              standout
+              bottom-slots
+              v-model="bairro"
+              label="Bairro"
+              hint="Bairro | Distrito "
+            >
+            </q-input>
+
+            <q-separator style="background-color: #96ceb4" />
+            <p></p>
+
+            <q-input
+              outlined
+              rounded
+              standout
+              bottom-slots
+              v-model="localidade"
+              label="Localidade"
+              hint="Cidade"
+            >
+            </q-input>
+
+            <q-separator style="background-color: #96ceb4" />
+            <p></p>
+
+            <q-input
+              class="uf"
+              outlined
+              rounded
+              standout
+              bottom-slots
+              v-model="uf"
+              label="UF"
+              hint="UF"
+            >
+            </q-input>
+          </div>
+          <div>
+            <q-btn
+              label="Limpar"
+              type="reset"
+              color="white"
+              flat
+              class="q-ml-sm"
+              style="background: #96ceb4"
+            />
+          </div>
+        </q-form>
+      </div>
+    </div>
+  </q-page>
+</template>
+<style>
+.input {
+  width: 30em;
+  margin: 10px;
+}
+.cep {
+  width: 15em;
+}
+.uf {
+  width: 8em;
+}
+</style>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+
+
+</script>
